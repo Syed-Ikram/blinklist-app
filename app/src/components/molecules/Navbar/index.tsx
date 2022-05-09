f130c0608fa93d14ee19aaf7772b11f576767f43
Ikram <syed.khaja.ikramuddin9@gmail.com>
2022-05-09 13:13:57 +0530

    changes by 1:1

--- app/src/components/molecules/Navbar/index.tsx
+++ app/src/components/molecules/Navbar/index.tsx
@@ -7,17 +7,17 @@ import ExploreTabs from "../ExploreTabs";
 const NavBar =()=>{
     
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    //const handleOpen = () => setOpen(true);
+    //const handleClose = () => setOpen(false);
     return(            
         <AppBar position="relative" sx={{ bgcolor: "white",color:"black","marginLeft":19}} elevation={0}>
             <Toolbar>
                 <Link underline="none" href="/explore" sx={{"marginTop":1}}><img src="http://127.0.0.1:8887/Blinklist.png" alt="blinklist"/></Link>
                 <Link underline="none" href="/explore" sx={{"marginLeft":2,"marginRight":2,"marginTop":1}}><Search /></Link>
-                <ButtonComponent variant="text" size="large" onClick={handleOpen}>Explore<KeyboardArrowDown /></ButtonComponent>
+                <ButtonComponent variant="text" size="large" onClick={() => setOpen(true)}>Explore<KeyboardArrowDown /></ButtonComponent>
                 <Modal
                     open={open}
-                    onClose={handleClose}
+                    onClose={() => setOpen(false)}
                     BackdropProps={{style: {top: "70px"}}}
                     disableScrollLock={true}
                 >
